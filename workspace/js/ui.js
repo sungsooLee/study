12496c39d21e8f73caf622aa94ee49e8cc17cf26
sgo1016939 <sgo1016939@mobmail.shinhan.com>
2022-02-09 17:18:52 +0900

    커밋 테스트

---
+++ workspace/js/ui.js
@@ -0,0 +1,17 @@
+
+
+$(function(){
+    scrollCheck();    
+});
+function scrollCheck(){
+    var lastScrollTop = 0;
+    $('.container').scroll(function(event){
+        var st = $(this).scrollTop();
+        if (st > lastScrollTop){
+            console.log('down')
+        } else {
+            console.log('up')
+        }
+        lastScrollTop = st;
+    });
+}
